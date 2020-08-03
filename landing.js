7e94a5e24d34c7c3cb0f682b2e52f415ca7537d4
fasasisherif <59504098+fasasisherif@users.noreply.github.com>
2020-08-03 17:09:46 +0100

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,16 @@
+var ham = document.getElementsByClassName("ham");
+var drop = document.getElementsByClassName("drop");
+
+ham[0].addEventListener ("click",myfunc)
+
+var a = 0;
+function myfunc (){
+drop[0].style.height="200px";
+drop[0].style.width="auto";		
+a++;
+
+if (a%2 === 0){
+drop[0].style.height="0px";
+drop[0].style.width="auto";				
+		}
+	}
